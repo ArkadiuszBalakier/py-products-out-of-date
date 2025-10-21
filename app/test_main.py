89f79cf555c484e3a9f53a03f9a3b5e57965449f
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-21 10:38:03 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+import datetime
+from unittest.mock import patch
+
+
+from app.main import outdated_products
+
+
+products = [
+    {
+        "name": "salmon",
+        "expiration_date": datetime.date(2022, 2, 10),
+        "price": 600
+    },
+    {
+        "name": "chicken",
+        "expiration_date": datetime.date(2022, 2, 5),
+        "price": 120
+    },
+    {
+        "name": "duck",
+        "expiration_date": datetime.date(2022, 2, 1),
+        "price": 160
+    }
+]
+
+
+def test_outdated_products_return_list_of_expaired_products_names() -> None:
+    with patch("app.main.datetime.date") as mock_date :
+        mock_date.today.return_value = datetime.date(2025, 1, 1)
+
+    assert outdated_products(products) == ["salmon", "chicken", "duck"]
+
+
+def test_product_with_expiration_date_equals_today_is_not_outdated() -> None:
+    with patch("app.main.datetime.date") as mock_date :
+        mock_date.today.return_value = datetime.date(2022, 2, 1)
+    assert outdated_products(products) == []
